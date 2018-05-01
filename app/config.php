b2e39e5d1a133c1b8173cd0021900da489b091fc
Aurino Junior <aurino_aj@hotmail.com>
2018-05-01 12:33:23 -0300

    Lamp

--- app/config.php
+++ app/config.php
@@ -1,11 +1,13 @@
 <?php
 
-	$dsn = "mysql:dbname=database;host=localhost";
+	$dsn = "mysql:dbname=teste;host=localhost";
 	$dbuser = "root";
 	$dbpass = "root";
 
 	try{
 		$pdo = new PDO($dsn,$dbuser, $dbpass);
+		echo "Conectado";
 	}catch (PDOException $e){
+		echo "Nao foi possivel conectar.<br>";
 		echo $e->getMessage();
 	}
